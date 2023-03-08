9f5d8053b54dd549e532d5dc78b14c84ba9163ea
changgun-park <pcg0537@gmail.com>
2023-03-08 16:07:24 +0900

    feat: add action on root

--- src/App.js
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <div>dfasdf</div>;
-}
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root, {
+  loader as rootLoader,
+  action as rootAction,
+} from "./routes/root";
+import { loader as contactLoader } from "./routes/contact";
+import ErrorPage from "./error-page";
+import Contact from "./routes/contact";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+        loader: contactLoader,
+      },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
